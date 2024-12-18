699d2ff12717f54e90bbdbf2c3b3b80fe2902389
ThomasGrare <thomasgrare64@gmail.com>
2024-12-18 10:07:32 +0100

    feat(auth): fix routes

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,3 +1,8 @@
-const usersRoutes = async (fastify, options) => {};
+import { login, register } from "../controllers/auth_controller.js";
+
+const usersRoutes = async (fastify, options) => {
+  fastify.get("/login", login);
+  fastify.get("/register", register);
+};
 
 export default usersRoutes;
