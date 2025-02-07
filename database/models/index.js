521773e0f74394d89cde53beda6299fb6df3b05e
ThomasGrare <thomasgrare64@gmail.com>
2025-02-07 21:46:59 +0100

    chore(api/sequelize/workspace) : sync models

--- database/models/index.js
+++ database/models/index.js
@@ -1,7 +1,9 @@
 import User from "./user.model.js";
+import Workspace from "./workspace.model.js";
 import Document from "./document.model.js";
 
 export const models = {
   User,
+  Workspace,
   Document,
 };
