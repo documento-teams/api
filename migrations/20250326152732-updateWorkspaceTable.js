fb88e8f77f4a7616c8bc6ef30270d3badccb226b
Rémy <remy.berjonneau@gmail.com>
2025-04-16 14:33:01 +0200

    fix retour pr

--- migrations/20250326152732-updateWorkspaceTable.js
+++ migrations/20250326152732-updateWorkspaceTable.js
@@ -3,7 +3,6 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    // Ajouter la colonne parentWorkspace
     await queryInterface.addColumn('workspaces', 'parentWorkspace', {
       type: Sequelize.INTEGER,
       allowNull: true,
@@ -13,15 +12,12 @@ module.exports = {
       },
     });
     
-    // Renommer la colonne userId en workspaceAuthor
     await queryInterface.renameColumn('workspaces', 'userId', 'workspaceAuthor');
   },
 
   async down (queryInterface, Sequelize) {
-    // Supprimer la colonne parentWorkspace
     await queryInterface.removeColumn('workspaces', 'parentWorkspace');
     
-    // Revenir au nom original de la colonne
     await queryInterface.renameColumn('workspaces', 'workspaceAuthor', 'userId');
   }
 };
